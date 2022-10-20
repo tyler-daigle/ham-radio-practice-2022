addf3e7ab624fa52d4a251c1db1a0c67f8684a99
Tyler D <tyler@tdaigle.com>
2022-10-20 04:29:05 -0400

    added check boxes to question choices

--- src/components/QuestionChoices.tsx
+++ src/components/QuestionChoices.tsx
@@ -1,13 +1,19 @@
+import styles from "../styles/QuestionChoices.module.css";
+import { useState } from "react";
+
 export default function QuestionChoices({ choices }) {
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
   return (
-    <ol
-      style={{
-        paddingLeft: "3rem",
-        margin: "1rem 0",
-      }}
-    >
-      {choices.map((choice) => (
-        <li>{choice}</li>
+    <ol className={styles.questionChoiceList}>
+      {choices.map((choice, index) => (
+        <li className={styles.choiceItem}>
+          <input
+            type="checkbox"
+            checked={selectedAnswer === index}
+            onChange={() => setSelectedAnswer(index)}
+          />
+          {choice}
+        </li>
       ))}
     </ol>
   );
